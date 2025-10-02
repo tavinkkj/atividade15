b0eec4cf9b5e742bd76b8f7ae79c411276409236
otavio <otavio.olivindo@aluno.ce.gov.br>
2025-10-02 14:46:20 -0300

    Add files via upload

---
+++ atividade15.py
@@ -0,0 +1,10 @@
+# Dada a tupla (10, 20, 30, 40, 50), mostre:
+# O primeiro elemento
+# O último elemento
+# A soma de todos os elementos
+
+tupla = (10, 20, 30, 40, 50)
+soma = sum(tupla)
+print("primeiro elemento:", tupla[0])
+print("último elemento:", tupla[4])
+print("soma:", soma)
